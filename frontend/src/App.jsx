522568cf0ef2e9db08c67e04a72b188a50d398af
Lama103130 <112017575+Lama103130@users.noreply.github.com>
2024-05-07 14:11:40 +0200

    router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,12 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { brukerInfo, filmInfo, sjangerInfo } from './Sanity/service'
-import Login from './component/Login'
+import React from 'react';
+import Login from './component/Login';
 
-function App() {
-  const [brukere, setBrukere] = useState ([])
-  const [filmer, setFilmer] = useState ([])
-  const [sjangere, setSjangere] = useState ([])
-
-  const fetchData = async () => {
-    const alleBrukere = await brukerInfo();
-    const alleFilmer = await filmInfo();
-    const alleSjangere = await sjangerInfo();
-
-    setBrukere(alleBrukere);
-    setFilmer(alleFilmer);
-    setSjangere(alleSjangere);
-
-    console.log("Brukere:", alleBrukere);
-    console.log("Filmer:", alleFilmer);
-    console.log("Sjangere:", alleSjangere);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [])
+const App = () => {
   return (
     <>
       <Login />
-      
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
